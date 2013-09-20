3dad522215752b086ed106f82d4f6792750e82fe
Piotr Zalewa <piotr@zalewa.info>
2013-09-20 12:22:47 +0200

    accelerometer registers, but is not responding

--- src/firefoxos/accelerometer.js
+++ src/firefoxos/accelerometer.js
@@ -1,44 +1,18 @@
 var firefoxos = require('cordova/platform');
 
-// initiate watching the acceleration and clear it straight afterwards
-function getCurrentAcceleration(accelerometerSuccess, accelerometerError) {
-    var listener = document.addEventListener('devicemotion', accelerationCallback, false);
-    function accelerationCallback(deviceMotionEvent) {
-        accelerometerSuccess(deviceMotionEvent.acceleration);
-        document.removeEventListener('devicemotion', listener, false);
-    }
-}
-
-// listen to acceleration and call accelerometerSuccess every x milliseconds
-function watchAcceleration(accelerometerSuccess, accelerometerError, options) {
-    // last timestamp when callback was called
-    var hit = 0;
-    var frequency = 0;
-    if (options && options.frequency) {
-        frequency = options.frequency;
-    }
-    function accelerationCallback(deviceMotionEvent) {
-        var newHit;
-        if (frequency) {
-          newHit = new Date().getTime();
-        }
-        if (!frequency || newHit >= hit + frequency) {
-            // return acceleration object instead of event
-            accelerometerSuccess(deviceMotionEvent.acceleration);
-            hit = newHit;
-        }
-    }
-    return document.removeEventListener('devicemotion', accelerationCallback, false);
-}
-
-function clearWatch(watchId) {
-    document.removeEventListener('devicemotion', watchId);
-}
 
 var Accelerometer = {
-    getCurrentAcceleration: getCurrentAcceleration,
-    watchAcceleration: watchAcceleration,
-    clearWatch: clearWatch,
+    start: function start(accelerometerSuccess, accelerometerError) {
+        console.log('start watching acecelerometer');
+        function accelerationCallback(deviceMotionEvent) {
+            accelerometerSuccess(deviceMotionEvent.acceleration);
+        }
+        return document.addEventListener('devicemotion', accelerationCallback, false);
+    },
+    stop: function stop(watchId) {
+        console.log('stop watching acecelerometer');
+        document.removeEventListener('devicemotion', watchId);
+    }
 };
 
 firefoxos.registerPlugin('Accelerometer', Accelerometer);
