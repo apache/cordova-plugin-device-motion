8a245a57cf90b2fb8c650103826b33d9b46b9b81
James Long <longster@gmail.com>
2013-10-24 19:15:29 -0400

    tweak scoping

--- src/firefoxos/accelerometer.js
+++ src/firefoxos/accelerometer.js
@@ -1,12 +1,15 @@
-var listener;
+
+function listener(success, ev) {
+    var acc = ev.accelerationIncludingGravity;
+    acc.timestamp = new Date().getTime();
+    success(acc);
+}
+
 var Accelerometer = {
     start: function start(success, error) {
-        listener = function(ev) {
-            var acc = ev.accelerationIncludingGravity;
-            acc.timestamp = new Date().getTime();
-            success(acc);
-        }
-        return window.addEventListener('devicemotion', listener, false);
+        return window.addEventListener('devicemotion', function(ev) {
+            listener(success, ev);
+        }, false);
     },
 
     stop: function stop() {
