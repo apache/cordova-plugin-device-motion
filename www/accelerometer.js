8f1ad44eec7ca768257faae92b1b447bf9b02dbb
Tim Barham <tim.barham@microsoft.com>
2015-07-29 10:19:43 +1000

    CB-9426 Fix exception when using device motion plugin on browser platform.
    The plugin's plugin.xml defines a general <js-module> with the name 'accelerometer'. It defines another <js-module> for the browser platform (that merges with the general module) with the same name. Modules with the same name is not allowed, so Cordova throws an exception at runtime when trying to define the browser version of the module.
    
    Since the only difference in the browser version of the module is to fake up devicemotion events, I've deleted the browser version and added that logic to the general module, with the following change: rather than starting a new event timer everytime watchAcceleration() is called (which could happen multiple times resulting in multiple superfluous timers), we have a single event timer going as long as there are active watchers.

--- www/accelerometer.js
+++ www/accelerometer.js
@@ -40,6 +40,9 @@ var listeners = [];
 // Last returned acceleration object from native
 var accel = null;
 
+// Timer used when faking up devicemotion events
+var eventTimerId = null;
+
 // Tells native to start.
 function start() {
     exec(function (a) {
@@ -161,6 +164,14 @@ var accelerometer = {
             start();
         }
 
+        if (cordova.platformId === "browser" && !eventTimerId) {
+            // Start firing devicemotion events if we haven't already
+            var devicemotionEvent = new Event('devicemotion');
+            eventTimerId = window.setInterval(function() {
+                window.dispatchEvent(devicemotionEvent);
+            }, 200);
+        }
+
         return id;
     },
 
@@ -175,6 +186,12 @@ var accelerometer = {
             window.clearInterval(timers[id].timer);
             removeListeners(timers[id].listeners);
             delete timers[id];
+
+            if (eventTimerId && Object.keys(timers).length === 0) {
+                // No more watchers, so stop firing 'devicemotion' events
+                window.clearInterval(eventTimerId);
+                eventTimerId = null;
+            }
         }
     }
 };
